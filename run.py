989fc59dfba0217c369e6304bcdfe8265ba4875f
Nathisaraujo <>
2023-11-16 10:46:33 +0000

    deleted user_option function

--- run.py
+++ run.py
@@ -32,27 +32,6 @@ def initial_page():
             print('Please, enter 1 or 2 to continue.')
             continue
 
-def user_option():
-    """
-    add option
-    """
-    while True:
-        try:
-            if user_option == "1":
-                user_details = input("Name and Surname:")
-                recipe_name = input("Name of the recipe:")
-                ingredients_list = input("What are the ingredients?")
-                preparation_step = input("How we prepare the recipe?")
-                food_type = input("Is it savoury or sweet?")
-                whos_favorite = input("This recipe is who's favorite?")
-                #update the worksheet and then initial menu again
-                break
-            elif user_option == "2":
-                give_idea = input("How would you like it? \n 1. Savoury \n 2. Sweet")
-                #give a random recipe
-                break
-        except VauleError as e:
-            print("Invalid option")
     
 
 

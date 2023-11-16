07ddc13e6ea7884b7733bdb7c1fed71d00304e49
Nathisaraujo <>
2023-11-16 10:45:57 +0000

    edited initial_page function

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,11 +17,20 @@ def initial_page():
     """
     Ask what the user wants to do
     """
-    print("What would you like to do? \n")
+    print("\nWhat would you like to do? \n")
     print("1. Check a recipe")
-    print("2. Add a new one")
-    user_option = input("Enter your answer here:")
-    user_option()
+    print("2. Add a new one\n")
+    
+    while True:
+        user_option = input("Enter your answer here:").strip()
+        if user_option == "1":
+            print("Ok! Let's do it!\n")
+            check_recipe()
+        elif user_option == "2":
+            add_recipe()
+        else:
+            print('Please, enter 1 or 2 to continue.')
+            continue
 
 def user_option():
     """
